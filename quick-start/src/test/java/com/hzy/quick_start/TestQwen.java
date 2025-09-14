1937e4e4d68471e6e1ecc2b5976a22c790645943
就在这这er <1799628141@qq.com>
2025-09-14 17:14:30 +0800

    阿里百炼 文生语音，语音转文本大模型调用快速入门

--- quick-start/src/test/java/com/hzy/quick_start/TestQwen.java
+++ quick-start/src/test/java/com/hzy/quick_start/TestQwen.java
@@ -1,21 +1,33 @@
 package com.hzy.quick_start;
 
+import com.alibaba.cloud.ai.dashscope.audio.DashScopeAudioTranscriptionModel;
+import com.alibaba.cloud.ai.dashscope.audio.DashScopeAudioTranscriptionOptions;
+import com.alibaba.cloud.ai.dashscope.audio.DashScopeSpeechSynthesisModel;
+import com.alibaba.cloud.ai.dashscope.audio.DashScopeSpeechSynthesisOptions;
+import com.alibaba.cloud.ai.dashscope.audio.synthesis.SpeechSynthesisPrompt;
+import com.alibaba.cloud.ai.dashscope.audio.synthesis.SpeechSynthesisResponse;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import com.alibaba.cloud.ai.dashscope.image.DashScopeImageModel;
 import com.alibaba.cloud.ai.dashscope.image.DashScopeImageOptions;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
 import reactor.core.publisher.Flux;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.ByteBuffer;
 
 
 @SpringBootTest
@@ -80,4 +92,61 @@ public class TestQwen {
 
     }
 
+    /**
+     * 文生语言
+     * @param speechSynthesisModel
+     * @throws IOException
+     */
+    // https://bailian.console.aliyun.com/?spm=5176.29619931.J__Z58Z6CX7MY__Ll8p1ZOR.1.74cd59fcXOTaDL&tab=doc#/doc/?type=model&url=https%3A%2F%2Fhelp.aliyun.com%2Fdocument_detail%2F2842586.html&renderType=iframe
+    @Test
+    public void testText2Audio(@Autowired DashScopeSpeechSynthesisModel speechSynthesisModel) throws IOException {
+        DashScopeSpeechSynthesisOptions options = DashScopeSpeechSynthesisOptions.builder()
+                //.voice()   // 人声
+                //.speed()    // 语速
+                //.model()    // 模型
+                //.responseFormat(DashScopeSpeechSynthesisApi.ResponseFormat.MP3)
+                .build();
+
+        SpeechSynthesisResponse response = speechSynthesisModel.call(
+                new SpeechSynthesisPrompt("大家好， 我是爱打篮球的蔡徐坤。",options)
+        );
+
+       // File file = new File( "C:\\Users\\17996\\Downloads\\mp3\\test.mp3");
+        File file = new File( "C:/Users/17996/Downloads/mp3/test1.mp3");
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            ByteBuffer byteBuffer = response.getResult().getOutput().getAudio();
+            fos.write(byteBuffer.array());
+        }
+        catch (IOException e) {
+            throw new IOException(e.getMessage());
+        }
+    }
+
+    /**
+     * 语言翻译
+     * @param transcriptionModel
+     * @throws MalformedURLException
+     */
+
+    private static final String AUDIO_RESOURCES_URL = "https://dashscope.oss-cn-beijing.aliyuncs.com/samples/audio/paraformer/hello_world_female2.wav";
+
+    @Test
+    public void testAudio2Text(
+            @Autowired
+            DashScopeAudioTranscriptionModel transcriptionModel
+    ) throws MalformedURLException {
+        DashScopeAudioTranscriptionOptions transcriptionOptions = DashScopeAudioTranscriptionOptions.builder()
+                //.withModel()   模型
+                .build();
+        AudioTranscriptionPrompt prompt = new AudioTranscriptionPrompt(
+                new UrlResource(AUDIO_RESOURCES_URL),
+                transcriptionOptions
+        );
+        AudioTranscriptionResponse response = transcriptionModel.call(
+                prompt
+        );
+
+        System.out.println(response.getResult().getOutput());
+
+    }
 }
