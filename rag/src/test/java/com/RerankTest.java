e2527a0ab2c35d190c3a50c4f773d99a574911db
就在这这er <1799628141@qq.com>
2025-09-20 22:55:41 +0800

    reg评估模型  快速入门

--- rag/src/test/java/com/RerankTest.java
+++ rag/src/test/java/com/RerankTest.java
@@ -4,10 +4,14 @@ import com.alibaba.cloud.ai.advisor.RetrievalRerankAdvisor;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import com.alibaba.cloud.ai.dashscope.embedding.DashScopeEmbeddingModel;
 import com.alibaba.cloud.ai.dashscope.rerank.DashScopeRerankModel;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.evaluation.FactCheckingEvaluator;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.evaluation.EvaluationRequest;
+import org.springframework.ai.evaluation.EvaluationResponse;
 import org.springframework.ai.reader.TextReader;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
@@ -18,7 +22,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
-
 import java.util.List;
 
 @SpringBootTest
@@ -77,4 +80,36 @@ public class RerankTest {
         System.out.println(content);
 
     }
+
+
+    /**
+     * 评估模型
+     * @param chatModel
+     */
+    @Test
+    void testFactChecking(@Autowired DashScopeChatModel chatModel,
+                          @Autowired VectorStore vectorStore) {
+
+
+        // 创建 FactCheckingEvaluator
+        var factCheckingEvaluator = new FactCheckingEvaluator(ChatClient.builder(chatModel));
+
+        // 示例上下文和声明
+        Document doc = Document.builder().text("地球是仅次于太阳的第三颗行星，也是已知唯一孕育生命的天文物体。").build();
+
+        List<Document> docList = List.of(doc);
+        String claim = "地球是距离太阳第三大行星。";
+
+        // 创建 EvaluationRequest
+        EvaluationRequest evaluationRequest = new EvaluationRequest(docList,claim);
+
+        // 执行评估
+        EvaluationResponse evaluationResponse = factCheckingEvaluator.evaluate(evaluationRequest);
+
+
+        Assertions.assertTrue(evaluationResponse.isPass(), "The claim should not be supported by the context");
+
+    }
+
+
 }
