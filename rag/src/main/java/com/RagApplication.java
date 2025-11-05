f7a80f002fb249fdde2a2c62a1ecdfba2cb145c6
就在这这er <1799628141@qq.com>
2025-11-05 20:13:09 +0800

    rag评估模型

--- rag/src/main/java/com/RagApplication.java
+++ rag/src/main/java/com/RagApplication.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RagApplication {
     public static void main(String[] args) {
         SpringApplication.run(RagApplication.class, args);
-        System.err.println("AI大模型启动~~~");
+        System.err.println("AI大模型启动~~");
     }
 }
